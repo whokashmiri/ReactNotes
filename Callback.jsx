ae43c08787eeb6997656340a87368211c241d325
Aaqib Bashir Mir <67498640+whokashmiri@users.noreply.github.com>
2024-12-04 22:45:20 +0530

    Add files via upload

---
+++ Callback.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/no-unknown-property */
+import {useCallback, useState} from 'react'
+import ChildComOfCallback from './ChildComOfCallback'
+
+const Callback = () => {
+    const [first, setfirst] = useState(0)
+    // function handleClick(){
+    //     setfirst(first+1)
+    // }
+    let handleClick=useCallback(()=>{
+        setfirst(first+1)
+    })
+  return (
+    <div>
+        <h3>Count: {first}</h3>
+        <button
+         onClick={handleClick}
+         handleClick = {handleClick}
+         >Increment</button>
+        <div>
+            <ChildComOfCallback buttonName = "Click"/>
+        </div>
+    </div>
+  )
+}
+
+export default Callback
---
+++ Effect.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+
+export const Effect = () => {
+    useEffect(() => {
+     console.log("Component Mounted");
+     
+    
+     
+    } )
+    
+  return (
+    <div>
+        <h1>useEffect</h1>
+        <h1>Something is here</h1>
+    </div>
+  )
+}
